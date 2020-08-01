5fa1b0e94ae96a2ce69c2cf60854d053e0d9f2eb
chilledmallow <64387479+chilledmallow@users.noreply.github.com>
2020-08-01 16:59:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.VYtsnWZZdvmm330QA5M0kodKF7fz8olB);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
